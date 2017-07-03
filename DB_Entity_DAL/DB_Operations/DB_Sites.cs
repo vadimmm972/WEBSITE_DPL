d53d3efa2bb9d517ccb53c0d25e5d79f6cc97812
Vadym Kobylynskiy <vadimmm972@gmail.com>
2017-07-03 23:04:16 +0300

    ADD info/ code / web site

---
+++ DB_Entity_DAL/DB_Operations/DB_Sites.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB_Entity_DAL.MedelsDataBase;
+
+namespace DB_Entity_DAL.DB_Operations
+{
+   public class DB_Sites
+    {
+        public string InsertRegion(Site sity)
+        {
+            try
+            {
+                Sell_BuyEntities db = new Sell_BuyEntities();
+                db.Sites.Add(sity);
+                db.SaveChanges();
+                return sity.name_sity + " was succefully inserted";
+            }
+            catch (Exception e)
+            {
+                return "Error:" + e.Message;
+            }
+        }
+
+        public string UpdateRegion(int id, Site sity)
+        {
+            try
+            {
+
+                Sell_BuyEntities db = new Sell_BuyEntities();
+                Site s = db.Sites.Find(id);
+                s.id_region = sity.id_region;
+                s.id_country = sity.id_country;
+                s.name_sity = sity.name_sity;
+
+                db.SaveChanges();
+                return s.name_sity + " was succefully updated";
+            }
+            catch (Exception e)
+            {
+                return "Error:" + e.Message;
+            }
+
+        }
+
+        public string DeleteRegion(int id)
+        {
+            try
+            {
+                Sell_BuyEntities db = new Sell_BuyEntities();
+                Site sity = db.Sites.Find(id);
+
+                db.Sites.Attach(sity);
+                db.Sites.Remove(sity);
+                db.SaveChanges();
+
+                return sity.name_sity + " was succefully delited";
+            }
+            catch (Exception e)
+            {
+                return "Error:" + e.Message;
+            }
+        }
+
+        public List<Site> GetallRegion()
+        {
+            try
+            {
+                using (Sell_BuyEntities db = new Sell_BuyEntities())
+                {
+                    List<Site> sity = (from x in db.Sites
+                                       select x).ToList();
+                    return sity;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ DB_Entity_DAL/DB_Operations/DB_User.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB_Entity_DAL.MedelsDataBase;
+
+namespace DB_Entity_DAL.DB_Operations
+{
+    public class DB_User
+    {
+        public string InsertUser(User user)
+        {
+            try
+            {
+                Sell_BuyEntities db = new Sell_BuyEntities();
+                db.Users.Add(user);
+                db.SaveChanges();
+                return user.name_last + "was succefully inserted";
+            }
+            catch (Exception e)
+            {
+
+                return "Error:" + e.Message;
+            }
+        }
+
+
+        public string UpdateUser(int id, User user)
+        {
+
+            try
+            {
+
+                Sell_BuyEntities db = new Sell_BuyEntities();
+                User u = db.Users.Find(id);
+                u.name_first = user.name_first;
+                u.name_last = user.name_last;
+                u.name_middle = user.name_middle;
+                u.phone = user.phone;
+                u.mail = user.mail;
+                u.id_country = user.id_country;
+                u.id_region = user.id_region;
+                u.id_sity = user.id_sity;
+                u.C_status = user.C_status;
+                u.active = user.active;
+                u.id_language = user.id_language;
+                u.C_image = user.C_image;
+                u.date_register = user.date_register;
+                u.C_login = user.C_login;
+                u.C_password = user.C_password;
+                db.SaveChanges();
+                return u.name_last + "was succefully updated";
+            }
+            catch (Exception e)
+            {
+                return "Error:" + e.Message;
+            }
+
+        }
+
+        public string DeleteUser(int id)
+        {
+            try
+            {
+                Sell_BuyEntities db = new Sell_BuyEntities();
+                User user = db.Users.Find(id);
+
+                db.Users.Attach(user);
+                db.Users.Remove(user);
+                db.SaveChanges();
+
+                return user.name_last + "was succefully delited";
+            }
+            catch (Exception e)
+            {
+                return "Error:" + e.Message;
+            }
+        }
+
+        public User GetUser(int id)
+        {
+            try
+            {
+                using (Sell_BuyEntities db = new Sell_BuyEntities())
+                {
+                    User user = db.Users.Find(id);
+                    return user;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public List<User> GetallUsers()
+        {
+            try
+            {
+                using (Sell_BuyEntities db = new Sell_BuyEntities())
+                {
+                    List<User> user = (from x in db.Users
+                                       select x).ToList();
+                    return user;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
